35e50c62fb2ad10ba98f6278e85da09a3729cd07
Deepak <212687506@ge.com>
2021-06-04 13:51:38 +0530

    sample jni implementation around libra

---
+++ src/main/java/io/github/dkrypt/JNAApp.java
@@ -0,0 +1,19 @@
+package io.github.dkrypt;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
+public class JNAApp {
+    public interface CryptoLib extends Library {
+        CryptoLib INSTANCE = Native.load("C:\\Users\\212687506\\Documents\\Workspace\\libra\\crypto.dll", CryptoLib.class);
+        void RefreshPassphrase();
+        Pointer GeneratePassphrase();
+        String ValidateToken(String token);
+    }
+    public static void main( String[] args )
+    {
+        CryptoLib myLib = CryptoLib.INSTANCE;
+        System.out.println(myLib.GeneratePassphrase());
+    }
+}
---
+++ src/main/java/io/github/dkrypt/JNIApp.java
@@ -0,0 +1,14 @@
+package io.github.dkrypt;
+
+public class JNIApp
+{
+    static {
+        System.load("C:\\Users\\212687506\\Documents\\Workspace\\libra\\jni-example\\lib\\crypto.dll");
+    }
+    public native String GeneratePassphrase();
+    public static void main( String[] args )
+    {
+        JNIApp myApp = new JNIApp();
+        System.out.println(myApp.GeneratePassphrase());
+    }
+}
